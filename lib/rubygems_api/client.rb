edc7471397905f1136a1a5a1cb4711233965688a
Kevin Kirsche <kevin.kirsche@verizon.com>
2015-04-02 15:48:01 -0400

    Simplify client

--- lib/rubygems_api/client.rb
+++ lib/rubygems_api/client.rb
@@ -1,5 +1,4 @@
 require 'rubygems_api/utilities'
-require 'rubygems_api/actions/gems'
 require 'hurley'
 require 'json'
 require 'yaml'
@@ -47,7 +46,7 @@ module Rubygems
         @api_key
       end
 
-      def total_downloads(format = 'json')
+      def rubygems_total_downloads(format = 'json')
         if validate_format(format)
           response = @client.get("downloads.#{format}")
           response = format_json_body(response) if format == 'json'
@@ -57,12 +56,85 @@ module Rubygems
         response
       end
 
-      def gems
-        @gems ||= Rubygems::API::Gems.new @client
+      def gem_info(name, format = 'json')
+        if validate_format(format)
+          response = @client.get("gems/#{name}.#{format}")
+          response = format_json_body(response) if format == 'json'
+          response = format_yaml_body(response) if format == 'yaml'
+        end
+
+        response
       end
 
-      def gem_versions
-        @gem_versions ||= Rubygems::API::Gems_Versions.new @client
+      def gem_search(query, format = 'json', args = { page: 1 })
+        if validate_format(format)
+          response = @client.get("search.#{format}", query: query, page: args[:page])
+          unless args[:skip_format]
+            response = format_json_body(response) if format == 'json'
+            response = format_yaml_body(response) if format == 'yaml'
+          end
+        end
+
+        response
+      end
+
+      def my_gems(format = 'json', args = {})
+        if validate_format(format)
+          response = @client.get("gems.#{format}")
+          unless args[:skip_format]
+            response = format_json_body(response) if format == 'json'
+            response = format_yaml_body(response) if format == 'yaml'
+          end
+        end
+
+        response
+      end
+
+      def submit_gem(gem_file)
+        response = @client.post("gems",
+          file: Hurley::UploadIO.new(gem_file.read, 'application/octet-stream'))
+
+        response
+      end
+
+      def yank_gem(gem_name, gem_version = nil, args = {})
+        response = @client.delete("gems/yank", {}.tap do |hash|
+          hash[:gem_name] = gem_name
+          hash[:gem_version] = gem_version unless gem_version.nil?
+          hash[:platform] = args[:platform] unless args[:platform].nil?
+        end)
+
+        response
+      end
+
+      def unyank_gem(gem_name, gem_version = nil, args = {})
+        response = @client.put("gems/unyank", {}.tap do |hash|
+          hash[:gem_name] = gem_name
+          hash[:gem_version] = gem_version unless gem_version.nil?
+          hash[:platform] = args[:platform] unless args[:platform].nil?
+        end)
+
+        response
+      end
+
+      def gem_versions(gem_name, format = 'json')
+        if validate_format(format)
+          response = @client.get("versions/#{gem_name}.#{format}")
+          response = format_json_body(response) if format == 'json'
+          response = format_yaml_body(response) if format == 'yaml'
+        end
+
+        response
+      end
+
+      def gem_downloads(gem_name, gem_version, format = 'json')
+        if validate_format(format)
+          response = @client.get("downloads/#{gem_name}-#{gem_version}.#{format}")
+          response = format_json_body(response) if format == 'json'
+          response = format_yaml_body(response) if format == 'yaml'
+        end
+
+        response
       end
     end
   end
